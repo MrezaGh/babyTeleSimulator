f3d683bf66729bfbe9c0263248c97e25f7f138ae
MrezaGh <33687449+MrezaGh@users.noreply.github.com>
2019-03-07 08:15:29 +0330

    Bullshit total bullshit2

--- app/src/main/java/com/td/mreza/babytelesimulator/StorageManager.java
+++ app/src/main/java/com/td/mreza/babytelesimulator/StorageManager.java
@@ -3,11 +3,17 @@ package com.td.mreza.babytelesimulator;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class StorageManager {
     Context context;
@@ -19,15 +25,32 @@ public class StorageManager {
         FileOutputStream outputStream;
         String contents = "hello world";
         try {
-            outputStream = context.openFileOutput("database", Context.MODE_PRIVATE);
-            outputStream.write(contents.getBytes());
-            outputStream.close();
-            FileInputStream input = context.openFileInput("database");
-            input.read();
+            outputStream = new FileOutputStream("database");
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            bufferedWriter.write(contents);
+            bufferedWriter.close();
+
+
+            FileInputStream fileInputStream = context.openFileInput("database");
+
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String lineData = bufferedReader.readLine();
+
+            Log.i("dataaaaa ", lineData);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            Log.d("fuckkk", "not found");
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d("fuckkk", "io exception");
         }
         Log.i("allfiles: ", String.valueOf(context.fileList()[1]));
     }
@@ -36,9 +59,17 @@ public class StorageManager {
     }
     public void load(){
         try {
-            FileInputStream inputStream = context.openFileInput("database");
-            int data = inputStream.read();
-            Log.i("file ->", String.valueOf(data));
+            FileInputStream fileInputStream = context.openFileInput("database");
+
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String lineData = bufferedReader.readLine();
+
+            Log.i("files -> ", lineData);
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             Log.i("oops", "file not found");
